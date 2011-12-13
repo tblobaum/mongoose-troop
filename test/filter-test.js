d07885963a39541d0bcb7be1d352b350bf7bff85
butu5 <butu25@gmail.com>
2011-12-13 13:08:38 +0530

    Added documentation and test case for the filter plugin

--- test/filter-test.js
+++ test/filter-test.js
@@ -1,33 +1,46 @@
-var mongoose = require('mongoose')
-var filter = require('../lib/filter')(mongoose,{debug:true})
+var vows = require('vows')
+  , assert = require('assert')
+  , mongoose = require('mongoose')
+  , filter = require('../lib/filter')
 
+// DB setup  
 mongoose.connect('mongodb://localhost/mongoose_troop')
 
+// Setting up test schema
 var Schema = mongoose.Schema
   , ObjectId = Schema.ObjectId
 
 var BlogPost = new Schema({
-    author : ObjectId
+    author : String
   , title  : String
   , body   : String
   , date   : Date
 })
 
+// Registering the filter plugin with mongoose
+// Note: Must be defined before creating schema object 
+mongoose.plugin(filter,{debug: true})
 
 var Blog = mongoose.model('BlogPost',BlogPost)
 
-var blog = new Blog({doesntBelong: 'filter this out'})
-blog.title = "Testing doesn't Belong Test case 3 with Filter"
+vows.describe('Filter unwanted attributes').addBatch({
+  'when attribute not defined in schema':{
+    topic: function(){
+        var blog = new Blog({doesntBelong: 'filter this out'})
+        blog.author = "butu5"
+        blog.title = "Hello mongoose troops!!!"
+        blog.save(this.callback)
+    },
 
-var options = {debug: true}
-blog.filter()
+    'it should not be stored in collection': function(topic){
+        Blog.findById(topic._id,function(err,post){
+          if(err) console.log('error while retrieving the post')
+          assert.equal(post.author, "butu5")
+          assert.equal(post.title, "Hello mongoose troops!!!")
+          assert.isUndefined(post.doesntBelong)
+          process.exit(0)//TODO I should not forcefully exit
+        })
+    }
+  }
 
-
-blog.save(function(err){
-  if(err) console.log('error')
-  console.log('successful..')
-  process.exit(0)
-})
-
-
-
+}).run()
