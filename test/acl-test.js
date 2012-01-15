f7a8edf37e8aba38f3fbc1b97cfe94fb05a16a39
Thomas Blobaum <tblobaum@gmail.com>
2012-01-15 01:15:23 -0600

    combined utils, added a simple acl plugin

---
+++ test/acl-test.js
@@ -0,0 +1,48 @@
+
+// Dependencies
+var util = require('util')
+  , assert = require('assert')
+  , mongoose = require('mongoose')
+  , acl = require('../lib/acl')
+  , common = require('./common')
+  , db = common.db
+  , cleanup = common.cleanup
+  , Schema = mongoose.Schema
+  , ObjectId = Schema.ObjectId
+
+// Run tests
+describe('acl', function() {
+  describe('#default()', function() {
+    var FooSchema = new Schema({})
+    FooSchema.plugin(acl)
+    var FooModel = mongoose.model('foo', FooSchema)
+    var foo = new FooModel()
+
+    foo.add_access('blog')
+    foo.add_access('dashboard')
+
+    it('should have access to blog', function(done) {
+      foo.access('blog', function (bool) {
+        assert.strictEqual(bool, true)
+        done()
+      })
+    })
+    
+    it('should have access to dashboard', function(done) {
+      foo.access('dashboard', function (bool) {
+        assert.strictEqual(bool, true)
+        done()
+      })
+    })
+      
+    it('should no longer have access to dashboard', function(done) {
+      foo.remove_access('dashboard')
+      foo.access('dashboard', function (bool) {
+        assert.strictEqual(bool, false)
+        done()
+      })
+    })
+        
+  })
+
+})
--- test/utils-test.js
+++ test/utils-test.js
@@ -1,50 +1,67 @@
 
 // Dependencies
+delete require('mongoose')
+
 var util = require('util')
   , assert = require('assert')
   , mongoose = require('mongoose')
-  , dbrefs = require('../lib/dbrefs')
+  , trooputils = require('../lib/utils')
   , common = require('./common')
   , db = common.db
   , cleanup = common.cleanup
   , Schema = mongoose.Schema
   , ObjectId = Schema.ObjectId
 
-// Run tests
-describe('dbrefs utility functions', function() {
-  describe('#default()', function() {
-  
-    mongoose.plugin(dbrefs)
     
-    var FooSchema = new Schema({
+// Run tests
+describe('troop utility functions', function() {
+  describe('#default()', function() {
+    var BarSchema = new Schema({
       other: {type:mongoose.Schema.ObjectId, ref:'other'},
       arr: [{type:mongoose.Schema.ObjectId, ref:'arr'}]
     })
     var OtherSchema = new Schema()
     var ArrSchema = new Schema()
     
-    var FooModel = mongoose.model('foo', FooSchema)
-    var OtherModel = mongoose.model('other', OtherSchema)
-    var ArrModel = mongoose.model('arr', ArrSchema)
+    trooputils(BarSchema)
+    mongoose.model('bar', BarSchema)
+    mongoose.model('other', OtherSchema)
+    mongoose.model('arr', ArrSchema)
     
+    var BarModel = mongoose.model('bar')
+    var OtherModel = mongoose.model('other')
+    var ArrModel = mongoose.model('arr')
     var othermodel = new OtherModel()
     var arrmodel = new ArrModel()
-    var foo = new FooModel({arr: [arrmodel], other: othermodel })
+    var bar = new BarModel({arr: [arrmodel], other: othermodel })
 
-    foo.arr.push(new ArrModel())
+    it('should have a method called merge', function(done) {
+      assert.ok(bar.merge)
+      done()
+    })
     
-    it('should return an object with the dbrefs ', function(done) {
-      foo.getdbrefs(function (refs) {
+    it('should have a method called removeDefaults', function(done) {
+      assert.ok(bar.removeDefaults)
+      done()
+    })
+    
+    it('should have a method called getdbrefs', function(done) {
+      assert.ok(bar.getdbrefs)
+      done()
+    })
+    
+    it('should return an object with the dbrefs', function(done) {
+      bar.getdbrefs(function (refs) {
         assert.notStrictEqual(Object.keys(refs), Array)
         done()
       })
     })
       
-    it('should still work with populate ', function(done) {
+    it('should still work with populate', function(done) {
       arrmodel.save(function (e) {
         othermodel.save(function (e) {
-          foo.save(function (e) {
-            FooModel
+          bar.save(function (e) {
+            BarModel
               .find()
               .populate('arr')
               .populate('other')
